45c07e0e2bbad0f315a736d45b8771b65c9ec580
Satyam Kumar <b22236@students.iitmandi.ac.in>
2025-05-11 00:57:22 +0530

    Warning Fix?

--- yolov7/mosaic.py
+++ yolov7/mosaic.py
@@ -377,7 +377,7 @@ class MosaicMixupDataset:
                     output_height,
                     output_width,
                     border_mode=0,
-                    value=pad_colour,
+                    border_value=pad_colour,
                 ),
                 A.Resize(height=output_height, width=output_width),
             ],
